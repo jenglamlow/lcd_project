fc6bdbe9eeaabb5c00c7aedace3a7e3c31291e8f
Jeng <jenglamlow@gmail.com>
2014-11-13 22:40:30 +0800

    initial code for main based on template

--- src/main.c
+++ src/main.c
@@ -1,3 +1,16 @@
+/*
+ * main.c
+ *
+ * Main Source file
+ *
+ */
+
+/*----------------------------------------------------------------------------*/
+/* Includes                                                                   */
+/*----------------------------------------------------------------------------*/
+
+/* Third party libraries include */
+#include <stdint.h>
 #include "inc/hw_gpio.h"
 #include "inc/hw_memmap.h"
 #include "inc/hw_sysctl.h"
@@ -5,12 +18,30 @@
 #include "driverlib/gpio.h"
 #include "driverlib/rom.h"
 #include "driverlib/sysctl.h"
- 
+
+/* Local includes */
+
+/*----------------------------------------------------------------------------*/
+/* Configuration                                                              */
+/*----------------------------------------------------------------------------*/
+
 #define LED_RED GPIO_PIN_1
 #define LED_BLUE GPIO_PIN_2
 #define LED_GREEN GPIO_PIN_3
- 
 
+/*----------------------------------------------------------------------------*/
+/* Private Types                                                              */
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/* Private Data                                                               */
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/* Helper Functions                                                           */
+/*----------------------------------------------------------------------------*/
+
+/* Error assertion definition when DEBUG is defined */
 #ifdef DEBUG
 void __error__(char *pcFilename, unsigned long ulLine)
 {
@@ -25,18 +56,36 @@ void __error__(char *pcFilename, unsigned long ulLine)
 }
 #endif
 
-int main() 
+/*----------------------------------------------------------------------------*/
+/* Event call-backs                                                           */
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/* IRQ Handlers                                                               */
+/*----------------------------------------------------------------------------*/
+
+
+/*----------------------------------------------------------------------------*/
+/* Services                                                                   */
+/*----------------------------------------------------------------------------*/
+
+
+/*----------------------------------------------------------------------------*/
+/* Main Routine                                                               */
+/*----------------------------------------------------------------------------*/
+
+int main()
 {
-  ROM_SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
-  ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-  ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN);
-  
-  for (;;) {
-    // set the red LED pin high, others low 
-    ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, LED_RED);
-    ROM_SysCtlDelay(5000000);
-    ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, 0); 
-    ROM_SysCtlDelay(5000000);
-  }
-  
+    ROM_SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN);
+
+    while (1)
+    {
+        ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, LED_RED);
+        ROM_SysCtlDelay(5000000);
+        ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, 0); 
+        ROM_SysCtlDelay(5000000);
+    }
 }
+
