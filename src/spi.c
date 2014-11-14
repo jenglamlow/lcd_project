731619ae9af38c60808e0e6a348a4af308df8d82
Jeng <jenglamlow@gmail.com>
2014-11-15 01:13:38 +0800

    update gitignore file to add vimfiles extension add spi file update makefile to add in spi file remove main.c

--- src/spi.c
+++ src/spi.c
@@ -1,7 +1,7 @@
 /*
- * main.c
+ * spi.c
  *
- * Main Source file
+ * Implementation file
  *
  */
 
@@ -11,23 +11,13 @@
 
 /* Third party libraries include */
 #include <stdint.h>
-#include "inc/hw_gpio.h"
-#include "inc/hw_memmap.h"
-#include "inc/hw_sysctl.h"
-#include "inc/hw_types.h"
-#include "driverlib/gpio.h"
-#include "driverlib/rom.h"
-#include "driverlib/sysctl.h"
 
 /* Local includes */
+#include "spi.h"
 
 /*----------------------------------------------------------------------------*/
 /* Configuration                                                              */
 /*----------------------------------------------------------------------------*/
-
-#define LED_RED GPIO_PIN_1
-#define LED_BLUE GPIO_PIN_2
-#define LED_GREEN GPIO_PIN_3
 
 /*----------------------------------------------------------------------------*/
 /* Private Types                                                              */
@@ -41,20 +31,6 @@
 /* Helper Functions                                                           */
 /*----------------------------------------------------------------------------*/
 
-/* Error assertion definition when DEBUG is defined */
-#ifdef DEBUG
-void __error__(char *pcFilename, unsigned long ulLine)
-{
-  //
-  // Something horrible happened! You need to look
-  // at file "pcFilename" at line "ulLine" to see
-  // what error is being reported.
-  //
-  while(1)
-  {
-  }
-}
-#endif
 
 /*----------------------------------------------------------------------------*/
 /* Event call-backs                                                           */
@@ -69,23 +45,16 @@ void __error__(char *pcFilename, unsigned long ulLine)
 /* Services                                                                   */
 /*----------------------------------------------------------------------------*/
 
-
-/*----------------------------------------------------------------------------*/
-/* Main Routine                                                               */
-/*----------------------------------------------------------------------------*/
-
-int main()
+static void spi_open(void)
 {
-    SysCtlClockSet(SYSCTL_SYSDIV_2_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN);
+}
 
-    while (1)
-    {
-        GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, LED_RED);
-        SysCtlDelay(5000000);
-        GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, 0); 
-        SysCtlDelay(5000000);
-    }
+/*----------------------------------------------------------------------------*/
+/* Initialisation                                                             */
+/*----------------------------------------------------------------------------*/
+
+void spi_init(spi_services_t *spi_services)
+{
+   spi_services->open = spi_open;
 }
 
