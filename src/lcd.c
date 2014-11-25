bdb64ea48c628379bb1e2bbe09450623b35b4c0e
unknown <jenglamlow@gmail.com>
2014-11-25 13:55:52 +0800

    Change tab indentation to space

--- src/lcd.c
+++ src/lcd.c
@@ -63,62 +63,62 @@
  *-----------------------------------------------------------------------------*/
 
 /* ILI9340 command */
-#define SWRESET		0x01
-#define	BSTRON		0x03
-#define RDDIDIF		0x04
-#define RDDST		0x09
-#define SLEEPIN         0x10
-#define	SLEEPOUT	0x11
-#define	NORON		0x13
-#define	INVOFF		0x20
-#define INVON      	0x21
-#define	SETCON		0x25
-#define DISPOFF         0x28
-#define DISPON          0x29
-#define CASETP          0x2A
-#define PASETP          0x2B
-#define RAMWRP          0x2C
-#define RGBSET	        0x2D
-#define	MADCTL		0x36
-#define SEP		0x37
-#define	COLMOD		0x3A
-#define DISCTR          0xB9
-#define DOR		0xBA
-#define	EC		0xC0
-#define RDID1		0xDA
-#define RDID2		0xDB
-#define RDID3		0xDC
+#define SWRESET     0x01
+#define BSTRON      0x03
+#define RDDIDIF     0x04
+#define RDDST       0x09
+#define SLEEPIN     0x10
+#define SLEEPOUT    0x11
+#define NORON       0x13
+#define INVOFF      0x20
+#define INVON       0x21
+#define SETCON      0x25
+#define DISPOFF     0x28
+#define DISPON      0x29
+#define CASETP      0x2A
+#define PASETP      0x2B
+#define RAMWRP      0x2C
+#define RGBSET      0x2D
+#define MADCTL      0x36
+#define SEP         0x37
+#define COLMOD      0x3A
+#define DISCTR      0xB9
+#define DOR         0xBA
+#define EC          0xC0
+#define RDID1       0xDA
+#define RDID2       0xDB
+#define RDID3       0xDC
 
-#define SETOSC		0xB0
-#define SETPWCTR4	0xB4
-#define SETPWCTR5	0xB5
-#define SETEXTCMD	0xC1
-#define SETGAMMAP	0xC2
-#define SETGAMMAN	0xC3
+#define SETOSC      0xB0
+#define SETPWCTR4   0xB4
+#define SETPWCTR5   0xB5
+#define SETEXTCMD   0xC1
+#define SETGAMMAP   0xC2
+#define SETGAMMAN   0xC3
 
 // ILI9340 specific
-#define ILIGS		0x26
-#define ILIMAC		0x36
-#define ILIFCNM		0xB1
-#define ILIFCIM		0xB2
-#define ILIFCPM		0xB3
-#define ILIDFC		0xB6
-#define ILIPC1		0xC0
-#define ILIPC2		0xC1
-#define ILIVC1		0xC5
-#define ILIVC2		0xC7
-#define PWRCTRLA	0xCB
-#define PWRCTRLB	0xCF
-#define RDID4		0xD3
-#define GER4SPI		0xD9
-#define ILIPGC		0xE0
-#define ILINGC		0xE1
-#define DTCTRLA1	0xE8
-#define DTCTRLA2	0xE9
-#define DTCTRLB		0xEA
-#define POSC		0xED
-#define ILIGFD		0xF2
-#define PRC		0xF7
+#define ILIGS       0x26
+#define ILIMAC      0x36
+#define ILIFCNM     0xB1
+#define ILIFCIM     0xB2
+#define ILIFCPM     0xB3
+#define ILIDFC      0xB6
+#define ILIPC1      0xC0
+#define ILIPC2      0xC1
+#define ILIVC1      0xC5
+#define ILIVC2      0xC7
+#define PWRCTRLA    0xCB
+#define PWRCTRLB    0xCF
+#define RDID4       0xD3
+#define GER4SPI     0xD9
+#define ILIPGC      0xE0
+#define ILINGC      0xE1
+#define DTCTRLA1    0xE8
+#define DTCTRLA2    0xE9
+#define DTCTRLB     0xEA
+#define POSC        0xED
+#define ILIGFD      0xF2
+#define PRC         0xF7
 
 /*-----------------------------------------------------------------------------
  *  Private Types
@@ -324,20 +324,20 @@ static void lcd_open(void)
     send_data(0x20); 
 
     send_command(ILIPC1);          /* power control */
-    send_data(0x23);   	
+    send_data(0x23);    
 
-    send_command(ILIPC2);    	
-    send_data(0x10);   	
+    send_command(ILIPC2);       
+    send_data(0x10);    
 
     send_command(ILIVC1);          /* VCOM Control */
-    send_data(0x3e);   	
+    send_data(0x3e);    
     send_data(0x28); 
 
     send_command(ILIVC2);    
-    send_data(0x86);  	 
+    send_data(0x86);     
 
-    send_command(MADCTL);    	/* Memory Access Control */
-    send_data(0x48);  	        /* Refresh Order - BGR colour filter */
+    send_command(MADCTL);       /* Memory Access Control */
+    send_data(0x48);            /* Refresh Order - BGR colour filter */
 
     send_command(COLMOD);          /* Pixel Format Set */
     send_data(0x55);               /* 16 bits/pixel */
@@ -346,18 +346,18 @@ static void lcd_open(void)
     send_data(0x00);               /* Fosc */
     send_data(0x18);               /* 24 clocks for line period */
 
-    send_command(ILIDFC);    	/* Display Function Control */
+    send_command(ILIDFC);       /* Display Function Control */
     send_data(0x08);               /* Interval Scan */
     send_data(0x82);               /* Normally Black , Scan cycle interval */
     send_data(0x27);  
 
-    send_command(ILIGFD);    	/* 3Gamma Function */
+    send_command(ILIGFD);       /* 3Gamma Function */
     send_data(0x00);               /* Disable 3G */
 
     send_command(ILIGS);           /* Gamma Set */
     send_data(0x01);               /* Gamma curve */
 
-    send_command(ILIPGC);    	/* Positive Gamma Correction */
+    send_command(ILIPGC);       /* Positive Gamma Correction */
     send_data(0x0F); 
     send_data(0x31); 
     send_data(0x2B); 
@@ -374,7 +374,7 @@ static void lcd_open(void)
     send_data(0x09); 
     send_data(0x00); 
 
-    send_command(ILINGC);    	/* Negative Gamma Correction */
+    send_command(ILINGC);       /* Negative Gamma Correction */
     send_data(0x00); 
     send_data(0x0E); 
     send_data(0x14); 
